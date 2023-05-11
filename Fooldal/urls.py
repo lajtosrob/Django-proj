18b9cf9c2fe11cf57b8de10e3672c7aa0e2c374d
lajtosrob <lajtosrobert@freemail.hu>
2023-05-11 15:26:40 +0200

    Django 15.video - HTML űrlapok mentése adatbázisba

--- Fooldal/urls.py
+++ Fooldal/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.fooldal),
+    path('', views.fooldal, name='fooldal'),
     #path('rolunk/', views.rolunk)
+    path('uj-szemely/', views.uj_szemely, name="uj-szemely")
 ]
--- Fooldal/views.py
+++ Fooldal/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from . models import Szemely
 
 # Create your views here.
@@ -17,5 +17,26 @@ def fooldal(request):
 
 
 def fooldal(request):
-    szemelyek = Szemely.objects.filter(keresztnev__contains="e")
+    szemelyek = Szemely.objects.all()
     return render(request, 'fooldal.html', {"szemelyek": szemelyek})
+
+def uj_szemely(request):
+    if request.method == "GET":
+        return render(request, 'uj_szemely.html')
+    elif request.method == "POST":
+        veznev = request.POST.get('veznev')
+        kernev = request.POST.get('kernev')
+        kor = request.POST.get('kor')
+        hazas = request.POST.get('hazas') # returns on or None
+        tortenet = request.POST.get('tortenet')
+
+        if hazas == 'on':
+            hazas = True
+        else:
+            hazas = False
+
+        szemely = Szemely(vezeteknev=veznev, keresztnev=kernev, eletkor=kor, hazas=hazas, elettortenet=tortenet)    
+        szemely.save()
+        
+
+        return redirect('fooldal')
