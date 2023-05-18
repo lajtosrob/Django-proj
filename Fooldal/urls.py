c55d5fe954c29253006ae10373436dde44f6b48d
lajtosrob <lajtosrobert@freemail.hu>
2023-05-18 14:34:10 +0200

    Django Video 19 - Bootstrap form

--- Fooldal/urls.py
+++ Fooldal/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.fooldal, name='fooldal'),
-    path('szemelyek', views.szemelyek, name='szemelyek'),
+    path('szemelyek/', views.szemelyek, name='szemelyek'),
     #path('rolunk/', views.rolunk)
     path('uj-szemely/', views.uj_szemely, name="uj-szemely"),
     path('rolunk/', views.rolunk, name="rolunk"),
